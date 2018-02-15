ee87186131c45f60a934130518590bd50ee9119a
Dmitry Petrov <dpetroff@gmail.com>
2018-02-16 00:26:53 +0100

    basic blog machinery

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('u/<username>/', views.BlogView.as_view(), name='blog'),
+    path('u/<username>/<int:publication_id>',
+         views.PublicationView.as_view(), name='publication'),
     path('update', views.update, name='update'),
 ]
--- web/views.py
+++ web/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 
-from .models import Publication, PublicationContent
+from .models import Publication, PublicationContent, User
 
 
 class IndexView(generic.ListView):
@@ -16,6 +18,51 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Publication.objects.order_by('-effective_pub_date')[:5]
+
+
+class BlogView(generic.ListView):
+    template_name = 'web/blog.html'
+    context_object_name = 'publications'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blog_username'] = self.kwargs['username']
+
+        return context
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+
+        try:
+            author = User.get(username=self.kwargs['username'])
+        except ObjectDoesNotExist:
+            raise Http404("Blog not found")
+
+        return Publication.objects.filter(author=author).order_by('-effective_pub_date')[:5]
+
+
+class PublicationView(generic.DetailView):
+    template_name = 'web/publication.html'
+    context_object_name = 'publication'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blog_username'] = self.kwargs['username']
+
+        return context
+
+    def get_object(self):
+        """Return the last five published questions."""
+
+        username = self.kwargs['username']
+        publication_id = self.kwargs['publication_id']
+
+        try:
+            publication = Publication.objects.filter(author__username=username, effective_pub_date__isnull=False).get(id=publication_id)
+        except ObjectDoesNotExist:
+            raise Http404("Post not found")
+
+        return publication
 
 
 @login_required
